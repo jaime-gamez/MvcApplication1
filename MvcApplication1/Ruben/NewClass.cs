0eeeec449c68fee6bb9fea02d8a17567c5119474
kobbee <kobbee13@gmail.com>
2013-12-06 14:25:32 -0600

    Se agregaron nuevos archivos

---
+++ MvcApplication1/Ruben/NewClass.cs
@@ -0,0 +1,15 @@
+﻿// Pruebas con una nueva clase
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Views.Class
+{
+   public class NewClass
+   {
+
+
+
+   }
+}
