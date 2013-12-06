41cd827309ccc2c4e036d27ae478c96ee974d758
kobbee <kobbee13@gmail.com>
2013-12-06 14:33:00 -0600

    Agregué nuevo archivo

---
+++ MvcApplication1/Ruben/Segunda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Ruben
+{
+   public class Segunda
+   {
+
+      // Esta la cree yo
+
+   }
+}
